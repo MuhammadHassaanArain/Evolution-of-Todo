8f7865785ef40c5b9223ef91aa316d0fdbcc92ce
Muhammad Hassaan <rafatnaz21@gmail.com>
2026-01-08 14:01:09 +0500

    Edited

--- phase-2/frontend/src/components/todo/empty-state.tsx
+++ phase-2/frontend/src/components/todo/empty-state.tsx
@@ -2,7 +2,9 @@
 
 import React from 'react'
 import { Button } from '../ui/button'
-import { cn } from '../../lib/utils'
+import { cn } from '@/lib/utils'
+
+
 
 export interface EmptyStateProps {
   title?: string
